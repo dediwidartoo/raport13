893870a66581a8b5a160a16735b09b6761eb4a00
dediwidartoo <dediwidarto78@gmail.com>
2017-10-03 19:52:36 +0700

    Update crud siswa

--- application/controllers/Siswa.php
+++ application/controllers/Siswa.php
@@ -49,16 +49,19 @@ class Siswa extends CI_Controller
                 'db' => 'id_siswa',
                 'dt' => 'aksi',
                 'formatter' => function($d) {
-                    return "<a href='edit.php?id=$d'>EDIT</a>";
+                    //return "<a href='edit.php?id=$d'>EDIT</a>";
+                    return anchor('siswa/edit/'.$d, '<i class="fa fa-edit"></i>', 'class="btn btn-info btn-xs" title="Edit"').'
+                    '.anchor('siswa/delete/'.$d, '<i class="fa fa-trash"></i>', 'class="btn btn-danger btn-xs" title="Delete" onclick="return konfirmasi_hapus()"').'
+                    '.anchor('siswa/detail/'.$d, '<i class="fa fa-list"></i>', 'class="btn btn-primary btn-xs" title="Detail Siswa"');
                 }
             ),
-            array(
+            /*array(
                 'db' => 'id_siswa',
                 'dt' => 'detail',
                 'formatter' => function($id) {
-                    return anchor('siswa/detail/'.$id, 'Detail');
+                    return anchor('siswa/detail/'.$id, '<i class="fa fa-list"></i>', 'class="btn btn-primary btn-xs" title="Detail Siswa"');
                 }
-            )
+            )*/
         );
         $sql_details = array(
             'user' => $this->db->username,
@@ -93,4 +96,66 @@ class Siswa extends CI_Controller
 			$this->template->load('template', 'siswa/add_v');
 		}
 	}
+
+    public function edit($id_siswa)
+    {
+        if (isset($_POST['submit'])) {
+            $this->Mdl_siswa->update();
+            redirect('siswa','refresh');
+        }
+        else{
+            $id             = $this->uri->segment(3);
+            //$data['siswa']  = $this->db->get_where('tbl_siswa', array('id_siswa' => $id))->row_array();
+            $where          = array('id_siswa' => $id_siswa );
+            $data['siswa']  = $this->Mdl_siswa->daftar_siswa($where, 'tbl_siswa')->result();
+            $this->template->load('template', 'siswa/edit_v', $data);
+        }
+    }
+
+    function update()
+    {
+        $this->load->model('Mdl_siswa', 'mod');
+        $data = array(
+            'id_siswa'          =>$this->input->post('id_siswa', TRUE),
+            'nis'               =>$this->input->post('nis', TRUE),
+            'nama_siswa'        =>$this->input->post('nama_siswa', TRUE),
+            'temp_lahir'        =>$this->input->post('temp_lahir', TRUE),
+            'tgl_lahir'         =>$this->input->post('tgl_lahir', TRUE),
+            'j_kelamin'         =>$this->input->post('j_kelamin', TRUE),
+            'agama'             =>$this->input->post('agama', TRUE),
+            'status_keluarga'   =>$this->input->post('status_keluarga', TRUE),
+            'anak_ke'           =>$this->input->post('anak_ke', TRUE),
+            'alamat'            =>$this->input->post('alamat', TRUE),
+            'telp'              =>$this->input->post('telp', TRUE),
+            'asal_sekolah'      =>$this->input->post('asal_sekolah', TRUE),
+            'kelas_diterima'    =>$this->input->post('kelas_diterima', TRUE),
+            'tgl_diterima'      =>$this->input->post('tgl_diterima', TRUE),
+            'nama_ayah'         =>$this->input->post('nama_ayah', TRUE),
+            'nama_ibu'          =>$this->input->post('nama_ibu', TRUE),
+            'alamat_orangtua'   =>$this->input->post('alamat_orangtua', TRUE),
+            'pekerjaan_ayah'    =>$this->input->post('pekerjaan_ayah', TRUE),
+            'pekerjaan_ibu'     =>$this->input->post('pekerjaan_ibu', TRUE),
+            'nama_wali'         =>$this->input->post('nama_wali', TRUE),
+            'alamat_wali'       =>$this->input->post('alamat_wali', TRUE),
+            'telp_wali'         =>$this->input->post('telp_wali', TRUE),
+            'pekerjaan_wali'    =>$this->input->post('pekerjaan_wali', TRUE),
+            'foto'              =>$this->input->post('foto', TRUE)
+        );
+        $this->mod->update($data);
+        redirect('siswa');
+    }
+
+    function delete()
+    {
+        $id = $this->uri->segment(3);
+        if (!empty($id)) {
+            # proses hapus
+            $this->db->where('id_siswa', $id);
+            $this->db->delete('tbl_siswa');
+            redirect('siswa','refresh');
+        }
+        else {
+            redirect('siswa','refresh');
+        }
+    }
 }
