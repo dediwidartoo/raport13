fb3c5330274e58782ad591d85a6e1276116ba0c1
dediwidartoo <dediwidarto78@gmail.com>
2017-10-03 22:34:23 +0700

    update new database

--- database/raport13.sql
+++ database/raport13.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Aug 24, 2017 at 02:39 PM
+-- Generation Time: Oct 03, 2017 at 05:24 PM
 -- Server version: 10.1.25-MariaDB
 -- PHP Version: 5.6.31
 
@@ -52,6 +52,30 @@ INSERT INTO `tabel_menu` (`id_menu`, `nama_menu`, `link`, `icon`, `is_main_menu`
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `tbl_agama`
+--
+
+CREATE TABLE `tbl_agama` (
+  `kd_agama` varchar(2) NOT NULL,
+  `nama_agama` varchar(25) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tbl_agama`
+--
+
+INSERT INTO `tbl_agama` (`kd_agama`, `nama_agama`) VALUES
+('01', 'ISLAM'),
+('02', 'KRISTEN / PROTESTAN'),
+('03', 'KATHOLIK'),
+('04', 'HINDU'),
+('05', 'BUDHA'),
+('06', 'KHONG HU CHU'),
+('99', 'LAIN-LAIN');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `tbl_siswa`
 --
 
@@ -60,16 +84,16 @@ CREATE TABLE `tbl_siswa` (
   `nis` varchar(15) NOT NULL,
   `nama_siswa` varchar(100) NOT NULL,
   `temp_lahir` varchar(15) NOT NULL,
-  `tgl_lahir` date NOT NULL,
+  `tgl_lahir` varchar(50) NOT NULL,
   `j_kelamin` enum('L','P') NOT NULL,
-  `agama` varchar(12) NOT NULL,
+  `kd_agama` varchar(2) NOT NULL,
   `status_keluarga` varchar(15) NOT NULL,
   `anak_ke` int(12) NOT NULL,
   `alamat` text NOT NULL,
   `telp` varchar(15) NOT NULL,
   `asal_sekolah` varchar(100) NOT NULL,
   `kelas_diterima` varchar(12) NOT NULL,
-  `tgl_diterima` date NOT NULL,
+  `tgl_diterima` varchar(50) NOT NULL,
   `nama_ayah` varchar(100) NOT NULL,
   `nama_ibu` varchar(100) NOT NULL,
   `alamat_orangtua` text NOT NULL,
@@ -86,9 +110,11 @@ CREATE TABLE `tbl_siswa` (
 -- Dumping data for table `tbl_siswa`
 --
 
-INSERT INTO `tbl_siswa` (`id_siswa`, `nis`, `nama_siswa`, `temp_lahir`, `tgl_lahir`, `j_kelamin`, `agama`, `status_keluarga`, `anak_ke`, `alamat`, `telp`, `asal_sekolah`, `kelas_diterima`, `tgl_diterima`, `nama_ayah`, `nama_ibu`, `alamat_orangtua`, `pekerjaan_ayah`, `pekerjaan_ibu`, `nama_wali`, `alamat_wali`, `telp_wali`, `pekerjaan_wali`, `foto`) VALUES
-(1, '141240000238', 'Dedi Widarto', 'Jepara', '1996-01-23', 'L', 'Islam', 'Kandung', 3, 'Sidigede', '089690427439', 'SD Sidigede', '1', '2017-08-07', 'bapak', 'ibuk', 'sidigede', 'tani', 'tani', 'waliku', 'sidigede', '089690427439', 'tani', 'foto.jpg'),
-(2, '141240000265', 'Muhammad Auliya Rochman', 'Jepara', '1995-08-17', 'L', 'Islam', 'Kandung', 2, 'Rajekwesi, Mayong, Jepara', '089690123456', 'SMP Terpadu Hadziqiyyah', '1', '2017-01-01', 'bapake auliya', 'ibuke auliya', 'rajekwesi', 'programmer', 'analyst', 'nama wali', 'rajekwesi', '089690432561', 'mebelan', '');
+INSERT INTO `tbl_siswa` (`id_siswa`, `nis`, `nama_siswa`, `temp_lahir`, `tgl_lahir`, `j_kelamin`, `kd_agama`, `status_keluarga`, `anak_ke`, `alamat`, `telp`, `asal_sekolah`, `kelas_diterima`, `tgl_diterima`, `nama_ayah`, `nama_ibu`, `alamat_orangtua`, `pekerjaan_ayah`, `pekerjaan_ibu`, `nama_wali`, `alamat_wali`, `telp_wali`, `pekerjaan_wali`, `foto`) VALUES
+(1, '141240000238', 'Dedi Widarto', 'Jepara', '1996-01-23', 'L', '01', 'Kandung', 3, 'Sidigede', '089690427439', 'SD Sidigede', '1', '03-10-2017', 'bapak', 'ibuk', 'sidigede', 'tani', 'tani', 'waliku', 'sidigede', '089690427439', 'tani', ''),
+(2, '141240000265', 'Muhammad Auliya Rochman', 'Jepara', '1995-08-17', 'L', '99', 'Kandung', 2, 'Rajekwesi, Mayong, Jepara', '089690123456', 'SMP Terpadu Hadziqiyyah', '1', '03-10-2017', 'bapake auliya', 'ibuke auliya', 'rajekwesi', 'programmer', 'analyst', 'nama wali', 'rajekwesi', '089690432561', 'mebelan', ''),
+(3, '141240000239', 'Ade Danvi Bagus Alip', 'Jepara', '2017-09-13', 'L', '03', 'Kandung', 1, 'Telukawur', '089', 'SMK slam Jepara', '', '03-10-2017', 'bapake danvi', 'ibuke danvi', 'Telukawur', 'Nelayan', 'Guru', 'bapake', 'telukawor', '089', 'nelayan', ''),
+(4, '141240000237', 'Rafy Irawan', 'Jepara', '07-11-1996', 'P', '05', 'Kandung', 2, 'asd', '089', 'SMK Islam Jepara', '2', '03-10-2017', 'bapake rafy', 'ibuke rafy', 'asd', 'Nelayan', 'Guru', 'bapake', 'asd', '089', 'nelayan', '');
 
 --
 -- Indexes for dumped tables
@@ -99,6 +125,12 @@ INSERT INTO `tbl_siswa` (`id_siswa`, `nis`, `nama_siswa`, `temp_lahir`, `tgl_lah
 --
 ALTER TABLE `tabel_menu`
   ADD PRIMARY KEY (`id_menu`);
+
+--
+-- Indexes for table `tbl_agama`
+--
+ALTER TABLE `tbl_agama`
+  ADD PRIMARY KEY (`kd_agama`);
 
 --
 -- Indexes for table `tbl_siswa`
@@ -119,7 +151,7 @@ ALTER TABLE `tabel_menu`
 -- AUTO_INCREMENT for table `tbl_siswa`
 --
 ALTER TABLE `tbl_siswa`
-  MODIFY `id_siswa` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;COMMIT;
+  MODIFY `id_siswa` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
